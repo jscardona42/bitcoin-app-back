4efbf1577ab48b4c4ac5afea04d87d76064c935b
jscardona42 <35873185+jscardona42@users.noreply.github.com>
2022-01-10 23:39:25 -0500

    Se contruye json con el histórico de 15 días

---
+++ src/bitcoins/historic/historic.ts
@@ -0,0 +1,147 @@
+/**
+ * json con el histórico del bitcoin hasta el día 10 de enero de 2022
+ */
+export let bitcoinsHistoric = [
+    {
+        year: "2021",
+        month: "12",
+        day: "29",
+        updated: "2021-12-29T23:59:00.000Z",
+        bpi_usd: { code: "USD", rate: 46461.7 },
+        bpi_euro: { code: "EUR", rate: 40931.5 },
+        created_at: "2021-12-29T23:59:00.478Z",
+        updated_at: "2021-12-29T23:59:00.478Z"
+    },
+    {
+        year: "2021",
+        month: "12",
+        day: "30",
+        updated: "2021-12-30T23:59:00.000Z",
+        bpi_usd: { code: "USD", rate: 47123.3 },
+        bpi_euro: { code: "EUR", rate: 41649.2 },
+        created_at: "2021-12-30T23:59:00.478Z",
+        updated_at: "2021-12-30T23:59:00.478Z"
+    },
+    {
+        year: "2021",
+        month: "12",
+        day: "31",
+        updated: "2021-12-31T23:59:00.000Z",
+        bpi_usd: { code: "USD", rate: 46219.5 },
+        bpi_euro: { code: "EUR", rate: 40657.4 },
+        created_at: "2021-12-31T23:59:00.478Z",
+        updated_at: "2021-12-31T23:59:00.478Z"
+    },
+    {
+        year: "2022",
+        month: "0",
+        day: "1",
+        updated: "2022-01-01T23:59:00.000Z",
+        bpi_usd: { code: "USD", rate: 47738.0 },
+        bpi_euro: { code: "EUR", rate: 42033.9 },
+        created_at: "2022-01-01T23:59:00.478Z",
+        updated_at: "2022-01-01T23:59:00.478Z"
+    },
+    {
+        year: "2022",
+        month: "0",
+        day: "2",
+        updated: "2022-01-02T23:59:00.000Z",
+        bpi_usd: { code: "USD", rate: 47311.8 },
+        bpi_euro: { code: "EUR", rate: 41642.7 },
+        created_at: "2022-01-02T23:59:00.478Z",
+        updated_at: "2022-01-02T23:59:00.478Z"
+    },
+    {
+        year: "2022",
+        month: "0",
+        day: "3",
+        updated: "2022-01-03T23:59:00.000Z",
+        bpi_usd: { code: "USD", rate: 46430.2 },
+        bpi_euro: { code: "EUR", rate: 41092.7 },
+        created_at: "2022-01-03T23:59:00.478Z",
+        updated_at: "2022-01-03T23:59:00.478Z"
+    },
+
+    {
+        year: "2022",
+        month: "0",
+        day: "4",
+        updated: "2022-01-04T23:59:00.000Z",
+        bpi_usd: { code: "USD", rate: 45837.3 },
+        bpi_euro: { code: "EUR", rate: 40653.9 },
+        created_at: "2022-01-04T23:59:00.478Z",
+        updated_at: "2022-01-04T23:59:00.478Z"
+    },
+    {
+        year: "2022",
+        month: "0",
+        day: "5",
+        updated: "2022-01-05T23:59:00.000Z",
+        bpi_usd: { code: "USD", rate: 43425.9 },
+        bpi_euro: { code: "EUR", rate: 38463.9 },
+        created_at: "2022-01-05T23:59:00.478Z",
+        updated_at: "2022-01-05T23:59:00.478Z"
+    },
+    {
+        year: "2022",
+        month: "0",
+        day: "6",
+        updated: "2022-01-06T23:59:00.000Z",
+        bpi_usd: { code: "USD", rate: 43097.5 },
+        bpi_euro: { code: "EUR", rate: 38189.9 },
+        created_at: "2022-01-06T23:59:00.478Z",
+        updated_at: "2022-01-06T23:59:00.478Z"
+    },
+    {
+        year: "2022",
+        month: "0",
+        day: "7",
+        updated: "2022-01-07T23:59:00.000Z",
+        bpi_usd: { code: "USD", rate: 41546.7 },
+        bpi_euro: { code: "EUR", rate: 33577.3 },
+        created_at: "2022-01-07T23:59:00.478Z",
+        updated_at: "2022-01-07T23:59:00.478Z"
+    },
+    {
+        year: "2022",
+        month: "0",
+        day: "8",
+        updated: "2022-01-08T23:59:00.000Z",
+        bpi_usd: { code: "USD", rate: 41672.0 },
+        bpi_euro: { code: "EUR", rate: 36738.3 },
+        created_at: "2022-01-08T23:59:00.478Z",
+        updated_at: "2022-01-08T23:59:00.478Z"
+    },
+    {
+        year: "2022",
+        month: "0",
+        day: "9",
+        updated: "2022-01-09T23:59:00.000Z",
+        bpi_usd: { code: "USD", rate: 41848.3 },
+        bpi_euro: { code: "EUR", rate: 36904.4 },
+        created_at: "2022-01-09T23:59:00.478Z",
+        updated_at: "2022-01-09T23:59:00.478Z"
+    },
+    {
+        year: "2022",
+        month: "0",
+        day: "10",
+        updated: "2022-01-10T23:59:00.000Z",
+        bpi_usd: { code: "USD", rate: 41832.2 },
+        bpi_euro: { code: "EUR", rate: 36945.3},
+        created_at: "2022-01-10T23:59:00.478Z",
+        updated_at: "2022-01-10T23:59:00.478Z"
+    },
+]
+
+export let bitcoinReturnNull = {
+    year: "0",
+    month: "0",
+    day: "0",
+    updated: new Date(),
+    bpi_usd: { code: "USD", rate: 0 },
+    bpi_euro: { code: "EUR", rate: 0 },
+    created_at: "2021-12-27T23:59:00.478Z",
+    updated_at: "2021-12-27T23:59:00.478Z"
+}
